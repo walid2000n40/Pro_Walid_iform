1ef830ff27ebb4616ac5d1f97da138f099efbaee
walid2000n40 <walid2000n40@gmail.com>
2026-04-08 11:39:30 +0400

    Revert DB path and refine responsive table layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Windowing;
 using ProWalid.Views;
 
 namespace ProWalid
@@ -9,6 +10,13 @@ namespace ProWalid
         {
             this.InitializeComponent();
             this.Title = "Pro Walid - نظام إدارة المعاملات";
+
+            if (this.AppWindow.Presenter is OverlappedPresenter presenter)
+            {
+                presenter.IsResizable = true;
+                presenter.IsMaximizable = true;
+                presenter.IsMinimizable = true;
+            }
             
             this.AppWindow.Resize(new Windows.Graphics.SizeInt32(1920, 1080));
             
